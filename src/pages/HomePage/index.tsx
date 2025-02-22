48bfdcde2da8a549c7e71e98e3e0eea7b3b9d759
PikuzaMarie <frag1lemaster@proton.me>
2025-02-23 02:34:12 +0300

    feat: create SectionLayout component to wrap section content

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,4 +1,5 @@
 import { PageLayout } from '../../components/PageLayout';
+import { SectionLayout } from '../../components/SectionLayout';
 
 export const HomePage: React.FC = () => {
   return (
@@ -6,6 +7,9 @@ export const HomePage: React.FC = () => {
       <h1 className="title">
         Let's find some <span className="title-accent">Art</span> here!
       </h1>
+      <SectionLayout subtitle="Artworks for you" title="Our special gallery">
+        <p>Some content here</p>
+      </SectionLayout>
     </PageLayout>
   );
 };
