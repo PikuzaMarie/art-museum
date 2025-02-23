1a3a91d94692f0c106262ec0077af7285c019e21
PikuzaMarie <frag1lemaster@proton.me>
2025-02-23 19:35:35 +0300

    feat: conditionally render favorite items or fallback content

--- src/pages/FavoritesPage/index.tsx
+++ src/pages/FavoritesPage/index.tsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { FavoritesContext } from '../../store';
 import { PageLayout } from '../../components/PageLayout';
 import bookmarkIcon from '../../assets/icons/bookmark-icon.svg';
 import { SectionLayout } from '../../components/SectionLayout';
+import { ArtworkCard } from '../../components/ArtworkCard';
 
 export const FavoritesPage: React.FC = () => {
+  const { favoriteArtworks } = useContext(FavoritesContext);
   return (
     <PageLayout isHomePage={false} className="favorites">
       <h1>
@@ -15,9 +20,20 @@ export const FavoritesPage: React.FC = () => {
           <span>favourites</span>
         </span>
       </h1>
-      <SectionLayout subtitle="Saved by you" title="Your favorites list">
-        <div>artworks here</div>
-      </SectionLayout>
+      {favoriteArtworks.length !== 0 ? (
+        <SectionLayout subtitle="Saved by you" title="Your favorites list">
+          {favoriteArtworks.map(item => (
+            <ArtworkCard key={item.id} artwork={item} />
+          ))}
+        </SectionLayout>
+      ) : (
+        <div>
+          <p>You haven't saved any artworks yet.</p>
+          <p>
+            Explore artworks at <Link to={`/`}>homepage</Link>
+          </p>
+        </div>
+      )}
     </PageLayout>
   );
 };
