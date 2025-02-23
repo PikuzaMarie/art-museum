d32b28fee6c609f6f72f066fe1358f9dcbf1c74c
PikuzaMarie <frag1lemaster@proton.me>
2025-02-23 19:48:18 +0300

    feat: add handler to FavoriteButton

--- src/components/FavoriteButton/index.tsx
+++ src/components/FavoriteButton/index.tsx
@@ -1,8 +1,28 @@
+import { useContext } from 'react';
+import { FavoritesContext } from '../../store';
+import { Artwork } from '../../types';
 import bookmarkIcon from '../../assets/icons/bookmark-icon.svg';
 
-export const FavoriteButton: React.FC = () => {
+interface FavoriteButtonProps {
+  artwork: Artwork;
+}
+
+export const FavoriteButton: React.FC<FavoriteButtonProps> = ({ artwork }) => {
+  const { favoriteArtworks, handleFavoriteAdd, handleFavoriteRemove } =
+    useContext(FavoritesContext);
+
+  const isFavorite = favoriteArtworks.some(item => item.id === artwork.id);
+
+  const handleFavoriteClick = () => {
+    if (isFavorite) {
+      handleFavoriteRemove(artwork.id);
+    } else {
+      handleFavoriteAdd(artwork);
+    }
+  };
+
   return (
-    <button className="button button-favorite">
+    <button onClick={handleFavoriteClick}>
       <img src={bookmarkIcon} alt="Bookmark icon" />
     </button>
   );
