ff4db9f9fdb3940a72edbdfc2758ac41b18bf8ca
PikuzaMarie <frag1lemaster@proton.me>
2025-02-23 14:33:03 +0300

    feat: add link to ArtworkPage

--- src/components/ArtworkCard/index.tsx
+++ src/components/ArtworkCard/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Artwork } from '../../types';
 import { FavoriteButton } from '../FavoriteButton';
 
@@ -8,27 +9,29 @@ interface ArtworkCardProps {
 export const ArtworkCard: React.FC<ArtworkCardProps> = ({ artwork }) => {
   return (
     <article className="artwork">
-      <figure className="artwork__image-container">
-        <img
-          src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
-          alt={artwork.thumbnail?.alt_text ?? artwork.title}
-          className="artwork__image"
-        />
-      </figure>
-      <div className="artwork__description">
-        <div className="artwork__heading">
-          <div>
-            <h5 className="artwork__title overflow">{artwork.title}</h5>
-            <span className="artwork__artist overflow">
-              {artwork.artist_title}
-            </span>
+      <Link to={`/artwork/${artwork.id}`}>
+        <figure className="artwork__image-container">
+          <img
+            src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
+            alt={artwork.thumbnail?.alt_text ?? artwork.title}
+            className="artwork__image"
+          />
+        </figure>
+        <div className="artwork__description">
+          <div className="artwork__heading">
+            <div>
+              <h5 className="artwork__title overflow">{artwork.title}</h5>
+              <span className="artwork__artist overflow">
+                {artwork.artist_title}
+              </span>
+            </div>
+            {artwork.is_public_domain ? (
+              <span className="artwork__is-public">Public</span>
+            ) : undefined}
           </div>
-          {artwork.is_public_domain ? (
-            <span className="artwork__is-public">Public</span>
-          ) : undefined}
+          <FavoriteButton />
         </div>
-        <FavoriteButton />
-      </div>
+      </Link>
     </article>
   );
 };
