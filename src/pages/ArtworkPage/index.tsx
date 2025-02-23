c8c6408ae780eb1eef942e81ff5d2a949e4c8b9c
PikuzaMarie <frag1lemaster@proton.me>
2025-02-23 15:13:57 +0300

    style: style ArtworkPage content

--- src/pages/ArtworkPage/index.tsx
+++ src/pages/ArtworkPage/index.tsx
@@ -11,42 +11,61 @@ export const ArtworkPage: React.FC = () => {
   const currentArtwork = artworks.find(item => item.id === Number(id));
 
   return (
-    <PageLayout isHomePage={false} className="artwork">
+    <PageLayout isHomePage={false} className="artwork-page">
       {currentArtwork && (
-        <article>
-          <div>
+        <article className="artwork-details">
+          <div className="artwork-details__image-container">
             <img
               src={`https://www.artic.edu/iiif/2/${currentArtwork.image_id}/full/843,/0/default.jpg`}
               alt={currentArtwork.thumbnail?.alt_text ?? currentArtwork.title}
+              className="artwork-details__image"
             />
-            <div>
+            <div className="artwork-details__button">
               <FavoriteButton />
             </div>
           </div>
-          <div>
-            <div>
-              <h2>{currentArtwork.title}</h2>
-              <span>{currentArtwork.artist_title}</span>
-              <span>
+          <div className="artwork-details__description">
+            <div className="artwork-details__heading">
+              <h2 className="artwork-details__title artwork-details__title_first">
+                {currentArtwork.title}
+              </h2>
+              <span className="artwork-details__artist">
+                {currentArtwork.artist_title}
+              </span>
+              <span className="artwork-details__reference-number">
                 {currentArtwork.main_reference_number.replace('.', '-')}
               </span>
             </div>
-            <div>
-              <h2>Overview</h2>
-              <ul>
-                <li>
-                  <span>Date:</span> {currentArtwork.date_display}
+            <div className="artwork-details__overview">
+              <h2 className="artwork-details__title">Overview</h2>
+              <ul className="artwork-details__list">
+                <li className="artwork-details__item">
+                  <span className="artwork-details__item_highlighted">
+                    Date:
+                  </span>{' '}
+                  {currentArtwork.date_display}
                 </li>
-                <li>
-                  <span>Place Of Origin:</span> {currentArtwork.place_of_origin}
+                <li className="artwork-details__item">
+                  <span className="artwork-details__item_highlighted">
+                    Place Of Origin:
+                  </span>{' '}
+                  {currentArtwork.place_of_origin}
                 </li>
-                <li>
-                  <span>Dimensions:</span> {currentArtwork.dimensions}
+                <li className="artwork-details__item">
+                  <span className="artwork-details__item_highlighted">
+                    Dimensions:
+                  </span>{' '}
+                  {currentArtwork.dimensions}
                 </li>
-                <li>
-                  <span>Repository:</span> {currentArtwork.credit_line}
+                <li className="artwork-details__item">
+                  <span className="artwork-details__item_highlighted">
+                    Repository:
+                  </span>{' '}
+                  {currentArtwork.credit_line}
                 </li>
-                {currentArtwork.is_public_domain ? <li>Public</li> : undefined}
+                {currentArtwork.is_public_domain ? (
+                  <li className="artwork-details__item">Public</li>
+                ) : undefined}
               </ul>
             </div>
           </div>
