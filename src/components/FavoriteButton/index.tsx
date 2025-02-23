53dc215e13ee04533fd4fbe5455b67986670e415
PikuzaMarie <frag1lemaster@proton.me>
2025-02-23 15:13:21 +0300

    style: set size params for FavouriteButton

--- src/components/FavoriteButton/index.tsx
+++ src/components/FavoriteButton/index.tsx
@@ -2,7 +2,7 @@ import bookmarkIcon from '../../assets/icons/bookmark-icon.svg';
 
 export const FavoriteButton: React.FC = () => {
   return (
-    <button className="button">
+    <button className="button button-favorite">
       <img src={bookmarkIcon} alt="Bookmark icon" />
     </button>
   );
