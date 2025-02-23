6fd8609c6c85728ff172f0ea42576945a964e59f
PikuzaMarie <frag1lemaster@proton.me>
2025-02-23 14:28:58 +0300

    feat: add route to ArtworkPage

--- src/pages/pagesData.tsx
+++ src/pages/pagesData.tsx
@@ -1,6 +1,8 @@
 import { HomePage } from './HomePage';
 import { RouterType } from '../types';
+import { ArtworkPage } from './ArtworkPage';
 
 export const pagesData: RouterType[] = [
   { path: '', element: <HomePage />, title: 'home' },
+  { path: '/artwork/:id', element: <ArtworkPage />, title: 'artwork' },
 ];
