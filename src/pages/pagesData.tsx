c8a356a1456b73f86731e656b9c53677ced7139a
PikuzaMarie <frag1lemaster@proton.me>
2025-02-23 18:48:02 +0300

    feat: add route to FavoritesPage

--- src/pages/pagesData.tsx
+++ src/pages/pagesData.tsx
@@ -1,8 +1,10 @@
 import { HomePage } from './HomePage';
 import { RouterType } from '../types';
 import { ArtworkPage } from './ArtworkPage';
+import { FavoritesPage } from './FavoritesPage';
 
 export const pagesData: RouterType[] = [
   { path: '', element: <HomePage />, title: 'home' },
   { path: '/artwork/:id', element: <ArtworkPage />, title: 'artwork' },
+  { path: '/favorites', element: <FavoritesPage />, title: 'favorites' },
 ];
