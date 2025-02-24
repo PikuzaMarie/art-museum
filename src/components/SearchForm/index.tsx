36918058c74cdd1232bfa5515415b34e5b073c32
PikuzaMarie <frag1lemaster@proton.me>
2025-02-24 16:26:54 +0300

    feat: validate SearchForm with 1.5 sec delay
    field is valid
    - send search reqest
    - show loading message and animation
    - update artworks state with search results
    
    field is invalid
    - don't send request
    - display error messages

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,32 +1,40 @@
 import { useState, useEffect, useContext } from 'react';
 import { useDebounce } from '../../hooks/useDebounce';
 import { fetchSearchResults } from '../../api';
+import { validateInput } from '../../utils/validationFunctions';
 
 import { ArtworksContext } from '../../store';
 
 export const SearchForm: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errors, setErrors] = useState<string[]>([]);
 
   const { setArtworks, isSearching, setIsSearching } =
     useContext(ArtworksContext);
+
+  const debouncedValidation = useDebounce({ value: searchTerm, delay: 1500 });
   const debouncedSearchTerm = useDebounce({
     value: searchTerm,
     delay: 500,
   });
 
   useEffect(() => {
-    if (debouncedSearchTerm) {
+    setErrors(validateInput(debouncedValidation.trim()));
+  }, [debouncedValidation]);
+
+  useEffect(() => {
+    const isValidInput = debouncedSearchTerm.length >= 3 && errors.length === 0;
+
+    if (isValidInput) {
       setIsSearching(true);
       fetchSearchResults(debouncedSearchTerm)
-        .then(results => {
-          setArtworks(results.data);
-        })
+        .then(results => setArtworks(results.data))
         .finally(() => setIsSearching(false));
     }
-  }, [debouncedSearchTerm]);
+  }, [debouncedSearchTerm, errors]);
 
   const handleSearchTermChange = (value: string) => {
-    setSearchTerm(value.trim());
+    setSearchTerm(value);
   };
 
   return (
@@ -34,10 +42,20 @@ export const SearchForm: React.FC = () => {
       <div>
         <input
           type="text"
+          placeholder="Search for Artwork, Artist..."
           value={searchTerm}
           onChange={e => handleSearchTermChange(e.target.value)}
+          maxLength={60}
         />
       </div>
+
+      {errors.length > 0 && (
+        <ul>
+          {errors.map((error, index) => (
+            <li key={index}>{error}</li>
+          ))}
+        </ul>
+      )}
       {isSearching && <p>Searching for results...</p>}
     </>
   );
