e61d04cbc70b032403069a30c5be0adaf3d0757a
PikuzaMarie <frag1lemaster@proton.me>
2025-02-24 10:58:06 +0300

    feat: create function to sort artworks based on criteria

---
+++ src/utils/sortArtworks/index.tsx
@@ -0,0 +1,38 @@
+import { Artwork } from '../../types';
+
+export const sortArtworks = (data: Artwork[], criteria: string) => {
+  const dataToSort = [...data];
+
+  switch (criteria) {
+    case 'title-asc':
+      return dataToSort.sort((a, b) => a.title.localeCompare(b.title));
+
+    case 'title-desc':
+      return dataToSort.sort((a, b) => b.title.localeCompare(a.title));
+
+    case 'artist-asc': {
+      return dataToSort.sort((a, b) => {
+        if (a.artist_title && b.artist_title) {
+          return a.artist_title.localeCompare(b.artist_title);
+        }
+        if (!a.artist_title && b.artist_title) return 1;
+        if (a.artist_title && !b.artist_title) return -1;
+        return 0;
+      });
+    }
+
+    case 'artist-desc': {
+      return dataToSort.sort((a, b) => {
+        if (a.artist_title && b.artist_title) {
+          return b.artist_title.localeCompare(a.artist_title);
+        }
+        if (!a.artist_title && b.artist_title) return 1;
+        if (a.artist_title && !b.artist_title) return -1;
+        return 0;
+      });
+    }
+
+    default:
+      return dataToSort;
+  }
+};
