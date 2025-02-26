504757898d845b1e1b5918e292cd97ea46d9c73d
PikuzaMarie <frag1lemaster@proton.me>
2025-02-26 13:44:50 +0300

    refactor: define error values as variables

--- src/utils/validationFunctions/index.ts
+++ src/utils/validationFunctions/index.ts
@@ -1,11 +1,14 @@
+export const isNotEmptyErrorValue = ['Input should have at least 3 characters'];
+export const hasLettersOrNumbersErrorValue = [
+  'Input should include letters or numbers',
+];
+
 export const isNotEmpty = (value: string) => {
-  return value.length >= 3 ? [] : ['Input should have at least 3 characters'];
+  return value.length >= 3 ? [] : isNotEmptyErrorValue;
 };
 
 export const hasLettersOrNumbers = (value: string) => {
-  return /[a-zA-Z0-9]/.test(value)
-    ? []
-    : ['Input should include letters or numbers'];
+  return /[a-zA-Z0-9]/.test(value) ? [] : hasLettersOrNumbersErrorValue;
 };
 
 export const validateInput = (value: string) => {
